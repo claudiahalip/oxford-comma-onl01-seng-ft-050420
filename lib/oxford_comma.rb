45f26e7ff1eea9d2de28184a6ed66a3db782fb9f
claudia v <claudia4ro@yahoo.com>
2020-05-08 13:59:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  if array.size == 1 
+    array.join
+  
+  elsif array.size == 2
+    array.join(" and ")
+    
+   
+  else
+    array.insert(array.size - 1, "and ")
+    last_elem = array.pop
+    string = array.join(", ")
+    string << last_elem
+    
+    
+  end
+  
 
 end
